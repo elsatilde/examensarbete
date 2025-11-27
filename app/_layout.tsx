912074be311550b7e23d47279049d440f0e924cf
elsatilde <kullaelsa@outlook.com>
2025-11-27 14:16:13 +0100

    User auth context and hooks; login and register with logic, error and loading

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,17 @@
 import { Stack } from 'expo-router'
 import { StatusBar } from "expo-status-bar"
 import React from 'react'
+import { UserProvider } from '../contexts/UserContext'
 
 export default function RootLayout() {
   return (
-    <>
+    <UserProvider>
       <StatusBar style="auto" />
       <Stack>
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="(dashboard)" options={{ headerShown: false }} />
       </Stack>
-
-    </>
+    </UserProvider>
   )
 }
 
