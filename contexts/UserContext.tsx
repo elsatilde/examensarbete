4816c7b7b2376cb866eea2cc6200be209dcc7b01
elsatilde <kullaelsa@outlook.com>
2025-12-01 14:31:08 +0100

    Used my auth and user types; Added confirm password input and logic;

--- contexts/UserContext.tsx
+++ contexts/UserContext.tsx
@@ -1,29 +1,39 @@
-import { auth } from "../services/firebase";
+import { auth, db } from "../services/firebase";
 import { 
     createUserWithEmailAndPassword, 
     signInWithEmailAndPassword, 
     signOut, 
     onAuthStateChanged,
  } from "firebase/auth";
+import { doc, serverTimestamp, setDoc } from "firebase/firestore";
 import { createContext, ReactNode, useEffect, useState } from "react";
+import { AppUser } from "../types/User.types";
 
 export type UserContextType = {
-    user: { uid: string; email: string | null } | null;
+    user: AppUser | null;
     loading: boolean;
     login: (email: string, password: string) => Promise<void>;
-    register: (email: string, password: string) => Promise<void>;
+    register: (email: string, password: string) => Promise<string>;
     logout: () => Promise<void>;
 }
 
 export const UserContext = createContext<UserContextType | undefined>(undefined);
 
 export function UserProvider({ children }: {children: ReactNode}) {
-    const [user, setUser] = useState<UserContextType["user"] | null>(null);
+    const [user, setUser] = useState<AppUser | null>(null);
     const [loading, setLoading] = useState(true);
 
 
     async function register(email: string, password: string) {
-        await createUserWithEmailAndPassword(auth, email, password);
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+        const uid = userCredential.user.uid;
+
+        await setDoc(doc(db, "users" , uid), {
+          email,
+          createdAt : serverTimestamp(),
+        });
+        
+        return uid;
     }
 
     async function login(email: string, password: string) {
