a6f402f5f9583b07ebe6a10b61ac6e882378eda3
elsatilde <kullaelsa@outlook.com>
2025-12-02 15:13:27 +0100

    Add garment button on ClosetPage; Back button to ClosetPage in AddGarment;

--- app/(dashboard)/closet.tsx
+++ app/(dashboard)/closet.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
-import { StyleSheet, Text } from 'react-native'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import ThemedView from '../../components/ThemedView'
 
 const Closet = () => {
   return (
     <ThemedView style={styles.container} safe={true}>
-        <Text style={styles.title}> Here you have your items </Text>
+        <Text style={styles.title}> Top </Text>
+        <ScrollView horizontal>
+        <View style={{ backgroundColor: 'pink', marginRight: 20, marginLeft: 20, width: 200, height: 200 }}></View>
+        <View style={{ backgroundColor: 'green', marginRight: 20, marginLeft: 20, width: 200, height: 200 }}></View>
+        <View style={{ backgroundColor: 'blue', marginRight: 20, marginLeft: 20, width: 200, height: 200 }}></View>
+        <View style={{ backgroundColor: 'red', marginRight: 20, marginLeft: 20, width: 200, height: 200 }}></View>
+        </ScrollView>
+        <Text style={styles.title}> Bottom </Text>
+        <ScrollView></ScrollView>
+        <Text style={styles.title}> Shoes </Text>
+        <ScrollView></ScrollView>
     </ThemedView>
   )
 }
@@ -15,11 +25,12 @@ export default Closet
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+        paddingLeft: 30,
+        paddingRight: 30,
+       
     },
     title: {
         fontWeight: 'bold',
-        fontSize: 18
+        fontSize: 15
     }
 })
